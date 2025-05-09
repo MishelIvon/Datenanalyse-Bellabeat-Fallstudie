5f121d4f31e05b29c5e498ce578025a35ab6cedc
MishelIvon <jovanovski.mi@web.de>
2025-05-10 00:57:12 +0200

    fitbit_analysis.sql

--- analysis/sql/fitbit_analysis.sql
+++ analysis/sql/fitbit_analysis.sql
@@ -1,14 +1,16 @@
--- Durchschnittliche tägliche Aktivität pro Nutzer
-SELECT
+-- Prüfe die Zeitspanne der Daten
+SELECT 
+  MIN(activity_date) AS start_date,
+  MAX(activity_date) AS end_date,
+  DATE_DIFF(MAX(activity_date), MIN(activity_date), DAY) AS days_covered
+FROM `bellabeat-analysis-459322.fitbit_data.cleaned_data`;
+
+-- Anzahl der Nutzer und Tage pro Nutzer
+SELECT 
   Id,
-  AVG(total_steps) AS avg_steps,
-  AVG(total_distance) AS avg_distance,
-  AVG(calories) AS avg_calories,
-  AVG(very_active_minutes) AS avg_very_active_min,
-  AVG(fairly_active_minutes)AS avg_fairly_active_min,
-  AVG(lightly_active_minutes) AS avg_lightly_active_min,
-  AVG(sedentary_minutes) AS avg_sedentary_min
+  COUNT(DISTINCT activity_date) AS days_tracked
 FROM `bellabeat-analysis-459322.fitbit_data.cleaned_data`
 GROUP BY Id
-ORDER BY avg_steps DESC;
+ORDER BY days_tracked DESC;
+
 
